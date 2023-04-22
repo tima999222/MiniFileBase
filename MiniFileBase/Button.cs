862ab1b642e50518e08278592b379406ac1c1e3c
Тимофей <121519813+tima999222@users.noreply.github.com>
2023-04-22 16:55:08 +0300

    Update Button.cs

--- MiniFileBase/Button.cs
+++ MiniFileBase/Button.cs
@@ -1,10 +1,3 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Policy;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace MiniFileBase
 {
     abstract public class Button : IRunnable, IButton
