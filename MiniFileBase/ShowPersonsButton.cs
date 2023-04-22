e08246c72bca7a58f7c525f56eaf8e969e028bdd
Тимофей <121519813+tima999222@users.noreply.github.com>
2023-04-22 16:53:28 +0300

    Update ShowPersonsButton.cs

--- MiniFileBase/ShowPersonsButton.cs
+++ MiniFileBase/ShowPersonsButton.cs
@@ -1,10 +1,5 @@
-﻿using Microsoft.SqlServer.Server;
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MiniFileBase
 {
